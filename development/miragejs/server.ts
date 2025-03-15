fc99eee27f5b57799a4abe8bc3e8cf88197b4f1a
Juan Manuel Diaz <juanmanueldiaz85@gmail.com>
2025-03-15 13:15:21 +1100

    added sorting to the stubbed hotels endpoint

--- development/miragejs/server.ts
+++ development/miragejs/server.ts
@@ -1,5 +1,6 @@
 import { createServer } from "miragejs"
 import * as data from "../data/hotels.json";
+import { Hotel } from "@custom-types/hotel";
 
 export const startMirage = () => createServer({
     logging: true,
@@ -7,8 +8,22 @@ export const startMirage = () => createServer({
     routes() {
         this.namespace = "api"
 
-        this.get("/hotels", () => {
-            return data;
+        this.get("/hotels", (_, request) => {
+            const sortOrder = request.queryParams.sortOrder;
+
+            const responseData = JSON.parse(JSON.stringify(data));
+
+            if (sortOrder === "price-high-to-low") {
+                responseData.results.sort((a: Hotel, b: Hotel) =>
+                    b.offer.displayPrice.amount - a.offer.displayPrice.amount
+                );
+            } else if (sortOrder === "price-low-to-high") {
+                responseData.results.sort((a: Hotel, b: Hotel) =>
+                    a.offer.displayPrice.amount - b.offer.displayPrice.amount
+                );
+            }
+
+            return responseData;
         })
     },
 })
